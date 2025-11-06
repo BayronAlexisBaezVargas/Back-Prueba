81da23620166d0d35afbc18b8d5dcf4f2d558c81
BayronAlexisBaezVargas <bay.baez@duocuc.cl>
2025-11-06 14:42:59 -0300

    Commit inicial de prueba de backend para una actividad en clases

--- src/main/java/com/example/crud/config/CorsConfig.java
+++ src/main/java/com/example/crud/config/CorsConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 
 public class CorsConfig {
-
+//a
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
