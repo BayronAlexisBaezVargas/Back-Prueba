6d0d851f1fd0d501fe9054c242435a36cb270bf7
BayronAlexisBaezVargas <bay.baez@duocuc.cl>
2025-11-06 14:42:14 -0300

    Commit inicial de prueba de backend para una actividad en clases

---
+++ src/main/java/com/example/crud/model/Persona.java
@@ -0,0 +1,31 @@
+package com.example.crud.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Persona {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String nombre;
+
+    //Getter y Setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    //Nombre
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+}
